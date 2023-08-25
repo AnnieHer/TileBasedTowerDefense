d214c3b8586c4ae08a03f6cb8883ad64ecf2a8bf
Annieher <anyabaka321@gmail.com>
2023-08-25 14:39:35 +0200

    AttackModifiers  + enemy barrier  + projectile manager + public methods to enemy manager

---
+++ Assets/Scripts/AttackModifiers/Multishot.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewAttackModifier", menuName = "Attack Modifiers/Multishot")]
+public class Multishot : AttackModifier
+{
+    public bool ProcModifiers;
+    public int targetCount;
+    public override AttackModifier Clone() {
+        return Instantiate(this);
+    }
+    public override void OnSendModifier(AttackData attackData) {
+        List<Enemy> enemies;
+        if (ProcModifiers) 
+        {
+            enemies = new List<Enemy>(
+                EnemyManager.Instance.GetClosestEnemy(
+                    attackData.towerLogic.transform.position, 
+                    attackData.towerLogic.GetRange() + 0.5f
+                )
+            );
+
+            enemies.Remove(attackData.target);
+            int i = 0;
+            foreach(Enemy enemy in enemies) {
+                if (i > targetCount) {
+                    return;
+                }
+                List<AttackModifier> clonedModifiers = new List<AttackModifier>();
+                foreach (AttackModifier modifier in attackData.attackModifierList)
+                {
+                    if (modifier == this) continue;
+                    clonedModifiers.Add(modifier.Clone());
+                }
+                AttackData _attackData = new AttackData(
+                    attackData.damage, 
+                    attackData.speed, 
+                    enemies[i], 
+                    attackData.damageType, 
+                    attackData.towerLogic, 
+                    clonedModifiers
+                );
+                i++;
+                ProjectileManager.instance.SpawnProjectile(_attackData, attackData.towerLogic.transform.position);
+            }
+        }
+        else 
+        {
+            enemies = new List<Enemy>(
+                EnemyManager.Instance.GetClosestEnemy(
+                    attackData.towerLogic.transform.position, 
+                    attackData.towerLogic.GetRange() + 0.5f
+                )
+            );
+            enemies.Remove(attackData.target);
+            int i = 0;
+            foreach(Enemy enemy in enemies) {
+                if (i > targetCount) {
+                    return;
+                }
+                AttackData _attackData = new AttackData(
+                    attackData.damage,
+                    attackData.speed, 
+                    enemies[i], 
+                    attackData.damageType, 
+                    attackData.towerLogic,
+                    new List<AttackModifier>()
+                );
+                i++;
+                ProjectileManager.instance.SpawnProjectile(_attackData, attackData.towerLogic.transform.position);
+            }
+        }
+    }
+    public override void OnDeliverModifier(AttackData attackData) {
+        //do smth when hit
+    }
+}
